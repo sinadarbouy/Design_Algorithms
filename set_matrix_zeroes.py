4833319ef2256137f543c2586003d3fd50d273b8
sinadarbouy <sinadarbouy@gmail.com>
2024-06-30 17:13:53 +0200

    O(1) space solution

--- set_matrix_zeroes.py
+++ set_matrix_zeroes.py
@@ -4,17 +4,32 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        m = len(matrix)
-        n = len(matrix[0])
-        zeros = []
-        for x in range(m):
-          for y in range(n):
-            if matrix[x][y] == 0:
-              zeros.append((x,y))
-        for x,y in zeros:
-          matrix[x][:]=[0] * n
-          for x_array in range(m):
-            matrix[x_array][y] = 0
+        m, n = len(matrix), len(matrix[0])
+        first_row_has_zero = any(matrix[0][j] == 0 for j in range(n))
+        first_col_has_zero = any(matrix[i][0] == 0 for i in range(m))
+        
+        # Use first row and column to mark zeros
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        
+        # Zero out cells based on marks in first row and column
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        
+        # Zero out first row if needed
+        if first_row_has_zero:
+            for j in range(n):
+                matrix[0][j] = 0
+        
+        # Zero out first column if needed
+        if first_col_has_zero:
+            for i in range(m):
+                matrix[i][0] = 0
     
 s = Solution()
 # matrix = [[1,1,1],[1,0,1],[1,1,1]]
