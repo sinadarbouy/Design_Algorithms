e2535681b3ef3f06963a4d675c093b50c4968a82
sinadarbouy <sinadarbouy@gmail.com>
2024-08-06 21:41:18 +0200

    added coundAndSay

--- countAndSay.py
+++ countAndSay.py
@@ -1,22 +1,25 @@
 
 class Solution:
-    def countAndSay(self, n: int) -> str:
-        strRLE="1"
+    def countAndSay(self, n: int) -> str: 
         if n == 1:
-          return strRLE
+          return "1"
+        def calcualteRLE(string: str) -> str:
+          RLE= []
+          i = 0
+          while i < len(string):
+            count = 1
+            while i +1 < len(string) and string[i] == string[i+1]:
+              i +=1
+              count +=1
+            RLE.append(f"{count}{string[i]}")
+            i+=1
+          return ''.join(RLE)
+              
+        strRLE="1"
         for _ in range(n-1):
-          strRLE = self.calcualteRLE(strRLE)
+          strRLE = calcualteRLE(strRLE)
         return strRLE 
-    def calcualteRLE(self,char: str) -> str:
-        RLE= ""
-        build = ""
-        for i in range(0, len(char)):
-            if i != len(char)-1 and char[i] == char[i+1]:
-                build += char[i]
-            else:
-                RLE+= str(len(build)+1) + char[i]
-                build = ""
-        return RLE
+
 
 s = Solution()
 print(s.countAndSay(4))
